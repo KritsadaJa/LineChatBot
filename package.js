6cad090196a7f0380a54545374207147596d3005
KritsadaJa <46640709+KritsadaJa@users.noreply.github.com>
2025-07-11 13:52:36 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "line-gemini-chatbot",
+  "version": "1.0.0",
+  "description": "LINE chatbot powered by Gemini API on Render",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "axios": "^1.7.2"
+  },
+  "engines": {
+    "node": "18.x"
+  }
+}
